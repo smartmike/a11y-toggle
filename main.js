4085193173eff89230bed00a7d38d3270ce4f272
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-03-08 11:00:55 +0100

    Improved script performance

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 (function () {
   var toggles = document.querySelectorAll('[data-a11y-toggle][aria-controls]');
-
-  function prepareDOM (toggle, target) {
-    toggle.hasAttribute('aria-expanded') || toggle.setAttribute('aria-expanded', false);
-    target.hasAttribute('aria-hidden') || target.setAttribute('aria-hidden', true);
-  }
-
-  function handleClick (toggle, target) {
-    var isExpanded = JSON.parse(toggle.getAttribute('aria-expanded'));
-
-    toggle.setAttribute('aria-expanded', !isExpanded);
-    target.setAttribute('aria-hidden', !!isExpanded);
-  }
+  var targetsList = [];
 
   for (var i = 0; i < toggles.length; i += 1) {
     var toggle = toggles[i];
-    var target = document.getElementById(toggle.getAttribute('aria-controls'));
-
-    prepareDOM(toggle, target);
-    toggle.addEventListener('click', handleClick.bind(null, toggle, target), false);
+    targetsList.push('#' + toggle.getAttribute('aria-controls'));
+    toggle.hasAttribute('aria-expanded') || toggle.setAttribute('aria-expanded', false);
   }
+
+  var targets = document.querySelectorAll(targetsList);
+  var targetsMap = {};
+
+  for (var j = 0; j < targets.length; j += 1) {
+    var target = targets[j];
+    targetsMap[target.id] = target;
+    target.hasAttribute('aria-hidden') || target.setAttribute('aria-hidden', true);
+  }
+
+  document.addEventListener('click', function (event) {
+    var toggle = event.target;
+    var target = targetsMap[toggle.getAttribute('aria-controls')];
+    var isExpanded = JSON.parse(toggle.getAttribute('aria-expanded'));
+
+    toggle.setAttribute('aria-expanded', !isExpanded);
+    target.setAttribute('aria-hidden', isExpanded);
+  });
 }());
